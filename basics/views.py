0a6b39816fc60cdd5f580115b9114e40a5c91a01
Piyush K Pandey <66120467+piyushkp03@users.noreply.github.com>
2021-06-19 13:23:07 +0530

    Update views.py

--- basics/views.py
+++ basics/views.py
@@ -103,7 +103,7 @@ def checkout(request):
             param_dict = {
 
                         'MID': 'yIjMtl03527914536186',
-                        'ORDER_ID': 'OrdIDD'+str(oid),
+                        'ORDER_ID': 'XXX'+str(oid),
                         'TXN_AMOUNT': str(prprice),
                         'CUST_ID': cartemail,
                         'INDUSTRY_TYPE_ID': 'Retail',
