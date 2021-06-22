5c017932e26064d9cbc1a9f945b07b0717edc5e1
Piyush K Pandey <66120467+piyushkp03@users.noreply.github.com>
2021-06-22 19:44:34 +0530

    Update views.py

--- basics/views.py
+++ basics/views.py
@@ -62,7 +62,12 @@ def buyfromcart():
 @csrf_exempt
 def handlerequest(request): 
     
-    global oid  
+    global oid   
+    oid=oid 
+    prname=prname 
+    prprice=prprice 
+    addr=addr 
+    ph=ph
     print("In handlerequest data's are,", cartorbuy1,usname,prname, prprice,addr,ph,oid)
    
     # paytm will send you post request here
